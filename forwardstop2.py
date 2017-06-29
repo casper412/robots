74613d55da21c33a06017bd9e41800fef57bdd30
casper412 <casper412@users.noreply.github.com>
2017-06-28 21:31:16 -0700

    test

--- forwardstop2.py
+++ forwardstop2.py
@@ -4,14 +4,13 @@ from time import sleep
 rr = RRB3(9,6)
 
 print("Go")
-range = rr.get_distance()
+
 rr.set_motors(1, 0, 1, 0) # both motors to full speed
 
-while(range > 30):
-	print("range: " + str(range))
+while(rr.get_distance() > 30):
+	print("range: " + str(rr.get_distance()))
 	sleep(0.05) # sleep for 50 milliseconds
-	range = rr.get_distance()
 	
 rr.set_motors(0, 0, 0, 0)
 print("Stop") 
-print("range: " + str(range))
+print("range: " + str(rr.get_distance()))

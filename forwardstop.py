4303741727ba055686d045038be36559e7735a36
casper412 <casper412@users.noreply.github.com>
2017-06-28 20:59:02 -0700

    hello

--- forwardstop.py
+++ forwardstop.py
@@ -3,7 +3,7 @@ from rrb3 import *
 rr = RRB3(9,6)
 
 print("Go")
-while(rr.get_distance)() == None or rr.get_distance() > 30):
+while(rr.get_distance() == None or rr.get_distance() > 30):
     print("range: " + rr.get_distance())
 	rr.forward(2,1)
 	

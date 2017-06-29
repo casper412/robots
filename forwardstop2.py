e4abf1aee44943666b84ca19a0762d49ec11de78
casper412 <casper412@users.noreply.github.com>
2017-06-28 21:39:31 -0700

    sleep

--- forwardstop2.py
+++ forwardstop2.py
@@ -7,13 +7,14 @@ print("Go")
 
 try:
 	rr.set_motors(1, 0, 1, 0) # both motors to full speed
-
-	while(rr.get_distance() > 30):
-		print("range: " + str(rr.get_distance()))
-		sleep(0.5) # sleep for 50 milliseconds
+    range = rr.get_distance()
+	while(range > 30):
+		print("range: " + str(range))
+		
+		range = rr.get_distance()
 	
 	rr.set_motors(0, 0, 0, 0)
 	print("Stop") 
-	print("range: " + str(rr.get_distance()))
+	print("range: " + str(range))
 finally:
 	rr.cleanup()

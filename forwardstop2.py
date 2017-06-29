f559faada0d159a11462f00aa953eb3bf765322b
casper412 <casper412@users.noreply.github.com>
2017-06-28 21:40:09 -0700

    test

--- forwardstop2.py
+++ forwardstop2.py
@@ -7,10 +7,9 @@ print("Go")
 
 try:
 	rr.set_motors(1, 0, 1, 0) # both motors to full speed
-    range = rr.get_distance()
+	range = rr.get_distance()
 	while(range > 30):
 		print("range: " + str(range))
-		
 		range = rr.get_distance()
 	
 	rr.set_motors(0, 0, 0, 0)

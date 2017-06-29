45dae5f3ad4934a586e2d63d997d271376d4c4fe
casper412 <casper412@users.noreply.github.com>
2017-06-28 21:31:40 -0700

    longer

--- forwardstop2.py
+++ forwardstop2.py
@@ -9,7 +9,7 @@ rr.set_motors(1, 0, 1, 0) # both motors to full speed
 
 while(rr.get_distance() > 30):
 	print("range: " + str(rr.get_distance()))
-	sleep(0.05) # sleep for 50 milliseconds
+	sleep(0.5) # sleep for 50 milliseconds
 	
 rr.set_motors(0, 0, 0, 0)
 print("Stop") 

2e0cbbceec9ee1f916262d173096d181b69abfa1
casper412 <casper412@users.noreply.github.com>
2017-06-28 20:51:59 -0700

    ultrasonic sensor

---
+++ forwardstop.py
@@ -0,0 +1,10 @@
+from rrb3 import *
+
+rr = RRB3(9,6)
+
+print("Go")
+while(rr.get_distance)() == None or rr.get_distance() > 30):
+    print("range: " + rr.get_distance())
+	rr.forward(2,1)
+	
+print("Stop")

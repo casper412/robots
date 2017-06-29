d05ce1b9851e48b9859dfabcee8b10c64c1023f2
casper412 <casper412@users.noreply.github.com>
2017-06-28 21:36:04 -0700

    add cleanup

--- forwardstop2.py
+++ forwardstop2.py
@@ -5,12 +5,15 @@ rr = RRB3(9,6)
 
 print("Go")
 
-rr.set_motors(1, 0, 1, 0) # both motors to full speed
+try:
+	rr.set_motors(1, 0, 1, 0) # both motors to full speed
 
-while(rr.get_distance() > 30):
-	print("range: " + str(rr.get_distance()))
-	sleep(0.5) # sleep for 50 milliseconds
+	while(rr.get_distance() > 30):
+		print("range: " + str(rr.get_distance()))
+		sleep(0.5) # sleep for 50 milliseconds
 	
-rr.set_motors(0, 0, 0, 0)
-print("Stop") 
-print("range: " + str(rr.get_distance()))
+	rr.set_motors(0, 0, 0, 0)
+	print("Stop") 
+	print("range: " + str(rr.get_distance()))
+finally:
+	rr.cleanup()

6878c6ef97228aa350023ddc79264633ce236d28
casper412 <casper412@users.noreply.github.com>
2017-07-01 20:27:59 -0700

    average of #3

--- forwardstop2.py
+++ forwardstop2.py
@@ -6,16 +6,21 @@ print("Go")
 
 try:
 	
-	range = rr.get_distance()
+	range = getRange(rr)
 	rr.set_motors(1, 0, 1, 0) # both motors to full speed
 	while(range > 30):
 		print("range: " + str(range))
-		range = rr.get_distance()
+		range = getRange(rr)
 		sleep(0.5)
-		range = rr.get_distance()
 	
 	rr.set_motors(0, 0, 0, 0)
 	print("Stop") 
 	print("range: " + str(range))
 finally:
 	rr.cleanup()
+	
+def getRange(rr):
+    sum = 0
+	for x in xrange(3):
+		sum = sum + rr.get_distance()
+	return sum / 3

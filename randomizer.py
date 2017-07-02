4521217f81aec8967eecb3f47221598a92419a34
casper412 <casper412@users.noreply.github.com>
2017-07-01 20:49:49 -0700

    test

--- randomizer.py
+++ randomizer.py
@@ -1,6 +1,12 @@
 from rrb3 import *
 from random import randint
 
+def getRange(rr):
+    sum = 0
+	for x in xrange(3):
+		sum = sum + rr.get_distance()
+	return sum / 3
+
 spintime = randint(10, 20)
  
 rr = RRB3(9,6)
@@ -12,22 +18,16 @@ rr.left(spintime,0.5)
 print("Go")
 
 try:
-	
 	range = getRange(rr)
 	rr.set_motors(0, 0, 0.5, 0) # continue spinning left
 	while(range > 60):
 		print("range: " + str(range))
 		range = getRange(rr)
 		sleep(0.5)
-	
+
 	rr.set_motors(0, 0, 0, 0)
 	print("Stop") 
 	print("range: " + str(range))
 finally:
 	rr.cleanup()
 	
-def getRange(rr):
-    sum = 0
-	for x in xrange(3):
-		sum = sum + rr.get_distance()
-	return sum / 3

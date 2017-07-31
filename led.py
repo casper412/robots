ce92323073653b3f0141331fed365735f4e3304e
Matthew Hall <mjhall@amazon.com>
2017-07-31 08:24:01 -0700

    print statements

--- led.py
+++ led.py
@@ -6,8 +6,10 @@ GPIO.setup(11, GPIO.OUT) ## Setup GPIO Pin 11 to OUT
 
 
 for i in xrange(1, 20):
+    print "Turning on %d" % i
     GPIO.output(11,True) ## Turn on GPIO pin 11
     time.sleep(1)
+    print "Turning off %d" % i
     GPIO.output(11,False) ## Turn off GPIO pin 11
 
 

e53160147c05f6698667a913e0a8346cdddfc2f1
Matthew Hall <mjhall@amazon.com>
2017-07-31 08:22:56 -0700

    blink

--- led.py
+++ led.py
@@ -1,4 +1,13 @@
 import RPi.GPIO as GPIO ## Import GPIO library
+import time
+
 GPIO.setmode(GPIO.BOARD) ## Use board pin numbering
 GPIO.setup(11, GPIO.OUT) ## Setup GPIO Pin 11 to OUT
-GPIO.output(11,True) ## Turn on GPIO pin 11
+
+
+for i in xrange(1, 20):
+    GPIO.output(11,True) ## Turn on GPIO pin 11
+    time.sleep(1)
+    GPIO.output(11,False) ## Turn off GPIO pin 11
+
+
